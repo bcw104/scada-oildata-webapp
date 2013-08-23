a6cc4354c86f97ee0c47f56a57caebb6a0651a0b
赵磊 <zhaolei-dlut@163.com>
2013-08-23 10:11:56 +0800

    注释掉redis

--- src/main/java/com/ht/scada/oildata/Scheduler.java
+++ src/main/java/com/ht/scada/oildata/Scheduler.java
@@ -56,8 +56,8 @@ public class Scheduler {
     @Autowired
     private TestSGTDao testSGTDao;
     @Autowired
-	private RealtimeDataService realtimeDataService;
-
+    private RealtimeDataService realtimeDataService;
+    
     private int interval = 20;  //存储间隔
 
     /**
@@ -68,7 +68,7 @@ public class Scheduler {
         List<EndTag> oilWellList = endTagService.getByType(EndTagTypeEnum.YOU_JING.toString());
         if (oilWellList != null && !oilWellList.isEmpty()) {
             for (EndTag endTag : oilWellList) {
-                
+
                 OilWellHourlyDataRecord oilWellHourlyDataRecord = scheduledService.getOilWellHourlyDataRecordByCode(endTag.getCode(), interval, new Date());
                 reportService.insertOilWellHourlyDataRecord(oilWellHourlyDataRecord);
                 System.out.println(new Date().toString() + "写入时记录" + endTag.getCode() + "成功！");
@@ -78,7 +78,7 @@ public class Scheduler {
         List<EndTag> waterList = endTagService.getByType(EndTagTypeEnum.SHUI_YUAN_JING.toString());
         if (waterList != null && !waterList.isEmpty()) {
             for (EndTag endTag : waterList) {
-                
+
                 WaterWellHourlyDataRecord record = scheduledService.getWaterWellHourlyDataRecordByCode(endTag.getCode(), interval, new Date());
                 reportService.insertWaterWellHourlyDataRecord(record);
                 System.out.println(new Date().toString() + "写入时记录" + endTag.getCode() + "成功！");
@@ -89,7 +89,7 @@ public class Scheduler {
         List<EndTag> gasList = endTagService.getByType(trqType);
         if (gasList != null && !gasList.isEmpty()) {
             for (EndTag endTag : gasList) {
-                
+
                 GasWellHourlyDataRecord record = scheduledService.getGasWellHourlyDataRecordByCode(endTag.getCode(), interval, new Date());
                 reportService.insertGasWellHourlyDataRecord(record);
                 System.out.println(new Date().toString() + "写入时记录" + endTag.getCode() + "成功！");
@@ -99,7 +99,7 @@ public class Scheduler {
         List<EndTag> zhuShuiList = endTagService.getByType(EndTagTypeEnum.ZHU_SHUI_ZHAN.toString());
         if (zhuShuiList != null && !zhuShuiList.isEmpty()) {
             for (EndTag endTag : zhuShuiList) {
-               
+
                 ZhuShuiHourlyDataRecord record = scheduledService.getZhuShuiHourlyDataRecordByCode(endTag.getCode(), interval, new Date());
                 reportService.insertZhuShuiHourlyDataRecord(record);
                 System.out.println(new Date().toString() + "写入时记录" + endTag.getCode() + "成功！");
@@ -176,50 +176,50 @@ public class Scheduler {
             }
         }
     }
-   
+
     @Scheduled(cron = "0 0/1 * * * ? ")
     public void testSGT() {
-    	
-    	Map<String, String> map = realtimeDataService.getEndTagVarGroupInfo("test_001", VarGroupEnum.YOU_JING_SGT.toString());
-		if(map != null) {
-			TestSGT testSGT = new TestSGT();
-			testSGT.setChongCheng(Float.valueOf(map.get(VarSubTypeEnum.CHONG_CHENG.toString().toLowerCase())));
-			testSGT.setChongCi(Float.valueOf(map.get(VarSubTypeEnum.CHONG_CI.toString().toLowerCase())));
-			testSGT.setChongCiShang(Float.valueOf(map.get(VarSubTypeEnum.SHANG_XING_CHONG_CI.toString().toLowerCase())));
-			testSGT.setChongCiXia(Float.valueOf(map.get(VarSubTypeEnum.XIA_XING_CHONG_CI.toString().toLowerCase())));
-			testSGT.setMinZaihe(Float.valueOf(map.get(VarSubTypeEnum.ZUI_XIAO_ZAI_HE.toString().toLowerCase())));
-			testSGT.setMaxZaihe(Float.valueOf(map.get(VarSubTypeEnum.ZUI_DA_ZAI_HE.toString().toLowerCase())));
-			
-			String weiyi = realtimeDataService.getEndTagVarYcArray("test_001" ,VarSubTypeEnum.WEI_YI_ARRAY.toString().toLowerCase());
-			String zaihe = realtimeDataService.getEndTagVarYcArray("test_001" ,VarSubTypeEnum.ZAI_HE_ARRAY.toString().toLowerCase());
-			
-			testSGT.setWeiyi(weiyi);
-			testSGT.setZaihe(zaihe);
-			testSGT.setDate(new Date());
-			float[] weiyiArray = String2FloatArrayUtil.string2FloatArrayUtil(weiyi, ",");
-			float[] zaiheArray = String2FloatArrayUtil.string2FloatArrayUtil(zaihe, ",");
-			float weiyiSum = 0, zaiheSum = 0;
-			for(float f : weiyiArray) {
-				weiyiSum += f;
-			}
-			for(float f : zaiheArray) {
-				zaiheSum += f;
-			}
-			testSGT.setWeiyiSum(weiyiSum);
-			testSGT.setZaiheSum(zaiheSum);
-			
-			
-			
-			testSGTDao.save(testSGT);
-			System.out.println("写入功图测试数据成功！");
-		}
-    	
-    	
-    	
+
+        Map<String, String> map = realtimeDataService.getEndTagVarGroupInfo("test_001", VarGroupEnum.YOU_JING_SGT.toString());
+        if (map != null) {
+            TestSGT testSGT = new TestSGT();
+            testSGT.setChongCheng(Float.valueOf(map.get(VarSubTypeEnum.CHONG_CHENG.toString().toLowerCase())));
+            testSGT.setChongCi(Float.valueOf(map.get(VarSubTypeEnum.CHONG_CI.toString().toLowerCase())));
+            testSGT.setChongCiShang(Float.valueOf(map.get(VarSubTypeEnum.SHANG_XING_CHONG_CI.toString().toLowerCase())));
+            testSGT.setChongCiXia(Float.valueOf(map.get(VarSubTypeEnum.XIA_XING_CHONG_CI.toString().toLowerCase())));
+            testSGT.setMinZaihe(Float.valueOf(map.get(VarSubTypeEnum.ZUI_XIAO_ZAI_HE.toString().toLowerCase())));
+            testSGT.setMaxZaihe(Float.valueOf(map.get(VarSubTypeEnum.ZUI_DA_ZAI_HE.toString().toLowerCase())));
+
+            String weiyi = realtimeDataService.getEndTagVarYcArray("test_001", VarSubTypeEnum.WEI_YI_ARRAY.toString().toLowerCase());
+            String zaihe = realtimeDataService.getEndTagVarYcArray("test_001", VarSubTypeEnum.ZAI_HE_ARRAY.toString().toLowerCase());
+
+            testSGT.setWeiyi(weiyi);
+            testSGT.setZaihe(zaihe);
+            testSGT.setDate(new Date());
+            float[] weiyiArray = String2FloatArrayUtil.string2FloatArrayUtil(weiyi, ",");
+            float[] zaiheArray = String2FloatArrayUtil.string2FloatArrayUtil(zaihe, ",");
+            float weiyiSum = 0, zaiheSum = 0;
+            for (float f : weiyiArray) {
+                weiyiSum += f;
+            }
+            for (float f : zaiheArray) {
+                zaiheSum += f;
+            }
+            testSGT.setWeiyiSum(weiyiSum);
+            testSGT.setZaiheSum(zaiheSum);
+
+
+
+            testSGTDao.save(testSGT);
+            System.out.println("写入功图测试数据成功！");
+        }
+
+
+
     }
-    
+
     private void sendFaultData(FaultDiagnoseRecord record) {
         String message = JSON.toJSONString(record);
-        redisTemplate.convertAndSend("FaultDiagnoseChannel", message);
+       // redisTemplate.convertAndSend("FaultDiagnoseChannel", message);
     }
 }
